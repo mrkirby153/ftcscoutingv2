a559a95d055b0a3aff5b2d25a7d70788c42425cc
mrkirby153 <mr.austinwhyte@gmail.com>
2017-09-23 17:13:53 -0700

    Hide the pending teams section if no invites are pending

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,18 +1,18 @@
 <router-link to="/dashboard" class="item" v-if="user">Dashboard</router-link>
 
 <div class="ui dropdown item" v-show="teams.length > 0 || pendingTeams.length > 0" tabindex="0">
-    Teams
+    Teams <div class="ui small circular label" v-if="pendingTeams.length > 0">@{{ pendingTeams.length }}</div>
     <i class="dropdown icon"></i>
     <div class="menu">
-        <div v-for="team in teams">
-            <router-link :to="'/team/'+team.id" class="item">Team @{{ team.team_number }}: @{{ team.name }}</router-link>
-        </div>
-        <div class="divider"></div>
-        <div class="header">
+        <router-link v-for="team in teams" :key="team.team_id" :to="'/team/'+team.team_id" class="item">Team
+            @{{ team.team_number }}: @{{ team.name }}
+        </router-link>
+        <div class="divider" v-if="pendingTeams.length > 0"></div>
+        <div class="header" v-if="pendingTeams.length > 0">
             Pending Invites
         </div>
-        <div v-for="team in pendingTeams">
-            <router-link :to="'/team/'+team.team_id" class="item">Team @{{ team.team_number }}: @{{ team.name }}</router-link>
-        </div>
+        <router-link v-for="team in pendingTeams" :key="team.team_id" :to="'/team/'+team.team_id" class="item">Team
+            @{{ team.team_number }}: @{{ team.name }}
+        </router-link>
     </div>
 </div>
