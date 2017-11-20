0e5cc22b4dc6f3beeaf911c5a7832e059d1c3835
mrkirby153 <mr.austinwhyte@gmail.com>
2017-11-19 16:46:50 -0800

    Add confirmation if the user leaves with unsaved changes

--- resources/assets/js/router/guards.js
+++ resources/assets/js/router/guards.js
@@ -1,11 +1,29 @@
 import router from './routes';
+import state from '../vuex/state'
+import {store} from '../vuex/vuex'
+import {SET_EDITING_QUESTION} from "../vuex/mutationTypes";
 
 router.beforeResolve(function (to, from, next) {
-    if(to.matched.some(record => record.meta.auth)){
-        if(User === null)
+    if (to.matched.some(record => record.meta.auth)) {
+        if (User === null)
             window.location.assign(route('login'));
         else
             next();
     }
     next();
+});
+
+router.beforeResolve(function (to, from, next) {
+    if (from.name === "survey.edit") {
+        if (state.editingQuestion !== null) {
+            if (confirm("You have unsaved changes. Navigating away will lose them")) {
+                next();
+                store.commit(SET_EDITING_QUESTION, null);
+            } else {
+                next(false);
+            }
+        }
+    } else {
+        next();
+    }
 });
