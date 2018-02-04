30ec700b2ee37e552ccb20e803691278ab574a53
mrkirby153 <mr.austinwhyte@gmail.com>
2018-02-03 20:14:06 -0800

    Remove debug log from super admin bypass trait

--- app/Policies/GrantsSuperAdmin.php
+++ app/Policies/GrantsSuperAdmin.php
@@ -10,7 +10,6 @@ trait GrantsSuperAdmin {
 
     function before(User $user, $ability) {
         if ($user->admin) {
-            \Log::info("Super admin bypass");
             return true;
         }
         return null;
